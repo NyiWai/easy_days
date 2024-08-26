f7f68e5f5026e5d75c8369b5a27f222f0da756fb
genius_allien <nyiwaiph29@gmail.com>
2024-08-26 21:15:05 +0630

    third commit

---
+++ Screens/Welcome.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {SafeAreaView,View, Text, Image} from 'react-native'
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
+
+const Welcome = () => {
+  return (
+    <SafeAreaView>
+        <View>
+            <Text>
+                Easy Days
+            </Text>
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default Welcome
